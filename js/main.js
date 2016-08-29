6a2beeded2e48c4882e9bbd13be91f143f624275
Vicky Firth <vickyfirth@Vickys-MacBook-Air.local>
2016-08-29 19:05:20 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('#design a').fluidbox();
+$('#logos a').fluidbox();
+$('#illust a').fluidbox();
+$('#web a').fluidbox();
+
+$(document).ready(function() {
+    $('.galleryBar').scrollToFixed();
+});
